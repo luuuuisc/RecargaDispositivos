95b6da67e3e33ae35abdf5afd1ac017d4a28a31b
Luiz Claudio <luizclaudio60a@gmail.com>
2024-06-03 01:09:32 -0300

    Busca paginada de produtos

--- src/main/java/com/algartelecom/RecargaDispositivos/controllers/ClientController.java
+++ src/main/java/com/algartelecom/RecargaDispositivos/controllers/ClientController.java
@@ -1,6 +1,8 @@
 package com.algartelecom.RecargaDispositivos.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +21,11 @@ public class ClientController {
 	@GetMapping(value = "/{id}")
 	public ClientDTO findById (@PathVariable Long id) {
 		return service.findById(id);
+	}
+	
+	@GetMapping
+	public Page<ClientDTO> findAll (Pageable pageable) {
+		return service.findAll(pageable);
 	}
 	
 }
--- src/main/java/com/algartelecom/RecargaDispositivos/services/ClientService.java
+++ src/main/java/com/algartelecom/RecargaDispositivos/services/ClientService.java
@@ -2,6 +2,8 @@ package com.algartelecom.RecargaDispositivos.services;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +21,12 @@ public class ClientService {
 	public ClientDTO findById(Long id) {
 		Client client = repository.findById(id).get();
 		return new ClientDTO(client);
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAll(Pageable pageable) {
+		Page<Client> result = repository.findAll(pageable);
+		return result.map(x -> new ClientDTO(x));
 	}
 	
 }
